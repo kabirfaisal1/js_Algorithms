0b95fc592815063605ad39aa9ee02b17dbb2eb0e
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-09 16:30:34 -0400

    added comment

--- medium/keyValue/keyValueStorage.js
+++ medium/keyValue/keyValueStorage.js
@@ -1,5 +1,6 @@
 // * How would you verify if existing data need to be updated 
 // * and add new data that are not part of the list?
+// <!--####################################################--!>
 // * stored data
 let baseData = [
     {
@@ -18,17 +19,17 @@ let baseData = [
         "id": "1234567",
         "subject": "cloud",
         "ide": "azure"
-    },{
+    }, {
         "id": "12345678",
         "subject": "code repositories",
         "ide": "gitLab"
-    },{
+    }, {
         "id": "12345679",
         "subject": "CI/CD tool",
         "ide": "gitAction"
     }
 ];
-// * data need to be add and modify
+// * data need to be compare by
 let incomingData = [
     {
         "id": "1234567",
@@ -46,20 +47,22 @@ let incomingData = [
         "id": "1234",
         "subject": "javaScript",
         "ide": "vs code"
-    },{
+    }, {
         "id": "12345678",
         "subject": "code repositories",
         "ide": "gitHub"
     }];
+// * function start here
 export function modifyKeyStorage(currentBaseData, updateBaseData) {
-    // ! let storedArr = Array.from(currentBaseData); NOT using 
-    // ! const newItem = Array.from(updateBaseData);
-    console.log("current baseData ~~>", currentBaseData)
+    // ? let storedArr = Array.from(currentBaseData); 
+    // ? const newItem = Array.from(updateBaseData); NOT using 
+    // * ^^ NOT using...need it if the data in import from .json file
+    console.log('current baseData ~~>', currentBaseData)
     currentBaseData = currentBaseData.map(e => (e = updateBaseData.find(a => a.id == e.id) || e, e));
     // console.log("updated data based on ID ~~>", storedArr);
-    currentBaseData.push(...updateBaseData.filter(compare => !currentBaseData.find(finalResutls => finalResutls === compare)))
-    console.log("new item stored to currentBaseData ~~>", currentBaseData);
+    currentBaseData.push(...updateBaseData.filter(compare => !currentBaseData.find(finalResults => finalResults === compare)))
+    console.log('new item stored to currentBaseData ~~>', currentBaseData);
 };
 
-
+// * using the function
 modifyKeyStorage(baseData, incomingData)
