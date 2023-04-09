7048ab60178d3f98fb2a538a79ec6bd8b78e9a46
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-09 05:46:45 -0400

    working on it

--- medium/arrayMapValue.js
+++ medium/arrayMapValue.js
@@ -1,38 +1,57 @@
-const testArray=[
+let storedData = [
     {
         "id": "1234",
         "subject": "javaScript",
-        "ide":"vs code"
-    },{
+        "ide": "vs code"
+    }, {
         "id": "12345",
         "subject": "c#",
-        "ide":"ms visual studio"
-    },{
+        "ide": "ms visual studio"
+    }, {
         "id": "123456",
         "subject": "sql",
-        "ide":"msSQL"
-    },{
+        "ide": "ms SQL"
+    }, {
         "id": "1234567",
         "subject": "cloud",
-        "ide":"azure"
+        "ide": "azure"
     }
 ];
-
-function arrayReplace(storedArray, argArray){
+let editData = [
+    {
+        "id": "1234567",
+        "subject": "cloud",
+        "ide": "aws"
+    }, {
+        "id": "123456",
+        "subject": "sql",
+        "ide": "my SQL"
+    }, {
+        "id": "12345678",
+        "subject": "automation tool",
+        "ide": "cypress"
+    }];
+function arrayUpdate(storedArray, argArray) {
     let storedArr = Array.from(storedArray);
-    storedArr = storedArr.map(v => v.id !== argArray.id ? v : argArray )
-    console.log("update with ~~>",storedArr)
-}
+    storedArr = storedArr.map(e => (e = argArray.find(a => a.id == e.id) || e, e));
+    //storedArr.map(v => v.id !== argArray.id ? v : argArray )
+    console.log("update with ~~>", storedArr)
+};
+function arrayAddData(storedArray, argArray) {
+    let storedArr = Array.from(storedArray);
+    let editData = Array.from(argArray);
+   
+    const elementExists = storedArr.includes(editData);
+    if (!elementExists) {
 
-let updateArray = [{
-    "id": "1234567",
-    "subject": "cloud",
-    "ide":"aws"
-},{
-    "id": "123456",
-    "subject": "sql",
-    "ide":"mySQL"
-}]
+        storedArr.push(editData);
+        
+        }
+        console.log(storedArr);
+    }
 
-arrayReplace(testArray,updateArray)
-// module.exports = new 
+arrayAddData(storedData, editData)
+
+
+
+//arrayUpdate(storedData,editData)
