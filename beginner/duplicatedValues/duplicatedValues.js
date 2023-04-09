1cf8088aee2d2cb0b4e162c6b318cd4227ad8887
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-09 19:43:26 -0400

    renameing branch and tested the code.

--- beginner/duplicatedValues/duplicatedValues.js
+++ beginner/duplicatedValues/duplicatedValues.js
@@ -1,23 +1,29 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-let arr = [1,2,1,3,4,5,1,3,7]
 
-var findDuplicatedValues = (nums)=> {
+let numArr = [1,2,1,3,4,5,1,3,7];
+let alpArr = ["a","b","c","a","d","e","f","a","c"];
+
+let stringArr = ["QA","DEV","BA","QA","PM","DEV","DEV","CEO","CTO"];
+var findDuplicatedValues = (value)=> {
     const temp = {};
-    return nums.filter(num => {
-        temp[num] = (temp[num] || 0) + 1;
-        return temp[num] > 1;
+    return value.filter(data => {
+        temp[data] = (temp[data] || 0) + 1;
+        return temp[data] > 1;
     })
 };
 
-var findNonDuplicatedValues = (arr) => 
-arr.filter((item,index) => {
-  arr.splice(index,1)
-  const unique = !arr.includes(item)
-  arr.splice(index,0,item)
+var findNonDuplicatedValues = (value) => 
+value.filter((item,index) => {
+  value.splice(index,1)
+  const unique = !value.includes(item)
+  value.splice(index,0,item)
   return unique
 })
-console.log("Duplicates: ",findDuplicatedValues(arr),
- "Non Duplicated Values: ",findNonDuplicatedValues(arr))
+console.log("number: Duplicates: ",findDuplicatedValues(numArr),
+ "number: Non Duplicated Values: ",findNonDuplicatedValues(numArr))
+
+console.log("abc Duplicates: ",findDuplicatedValues(alpArr),
+"abc Non Duplicated Values: ",findNonDuplicatedValues(alpArr))
+
+
+console.log("abc Duplicates: ",findDuplicatedValues(stringArr),
+"abc Non Duplicated Values: ",findNonDuplicatedValues(stringArr))
