c857a925f3fecb1afbc6f9a8e17d7a19188e869a
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-09 21:02:28 -0400

    array count is working but need to fix word

--- beginner/duplicatedValues/duplicatedValues.js
+++ beginner/duplicatedValues/duplicatedValues.js
@@ -1,10 +1,12 @@
 
-let numArr = [1, 2, 1, 3, 4, 5, 1, 3, 7];
-let alpArr = ["a", "b", "c", "a", "d", "e", "f", "a", "c"];
-let stringArr = ["QA", "DEV", "BA", "QA", "PM", "DEV", "DEV", "CEO", "CTO"];
+const numArr = [1, 2, 1, 3, 4, 5, 1, 3, 7];
+const alpArr = ["a", "b", "c", "a", "d", "e", "f", "a", "c"];
+const stringArr = ["QA", "DEV", "BA", "QA", "PM", "DEV", "DEV", "CEO", "CTO"];
 
-const strWprd = "Hand, Hand, Fingers, Thumb, one thumb one thumb drumming on a drum";
+const strWord = "Hand, Hand, Fingers, Thumb, one thumb one thumb drumming on a drum";
 const str2 = "123 4516 713 123 76887 713";
+const strletters = "Faisal";
+
 var findDuplicatedValues = (value) => {
   let arrayFlag= Array.isArray(value)
   if(arrayFlag){
@@ -27,25 +29,61 @@ var findDuplicatedValues = (value) => {
   }
 };
 
-var findNonDuplicatedArrayValues = (value) =>{
-
+var findNonDuplicatedValues = (value) =>{
+  let arrayFlag= Array.isArray(value)
+  if(arrayFlag){
+    let storedArr = Array.from(value); 
+  const temp = {};
+  let count= 0
  return value.filter((item, index) => {
     value.splice(index, 1)
     const unique = !value.includes(item)
     value.splice(index, 0, item)
     return unique
-  })
-}
+  })}else{
+    let strArr = value.split(" ");
+    let res = [];
+   
+    for (let i = 0; i < strArr.length; i++) {
+      if (strArr.indexOf(strArr[i]) === strArr.lastIndexOf(strArr[i])) {
+        if (!res.includes(strArr[i])) {
+          res.push(strArr[i]);
+        };
+      };
+    };
+    return res.join(" ");
+  }
+};
 
 console.log("number: Duplicates: ", findDuplicatedValues(stringArr),
-  "number: Non Duplicated Values: ", findNonDuplicatedArrayValues(stringArr))
+  "number: Non Duplicated Values: ", findNonDuplicatedValues(stringArr))
   
-  console.log("word: Duplicates: ", findDuplicatedValues(strWprd))
+console.log("word: Duplicates: ", findDuplicatedValues(strWord), "number: Non Duplicated Values: ",findNonDuplicatedValues(strWord))
 
-// console.log("abc Duplicates: ", findDuplicatedArrayValues(alpArr),
-//   "abc Non Duplicated Values: ", findNonDuplicatedArrayValues(alpArr))
+var countDuplicatedValues = (value) => {
+  let arrayFlag= Array.isArray(value)
+  let elementCounts;
+  if(arrayFlag){
+    let storedArr = Array.from(value); 
+   
+    return elementCounts = storedArr.reduce((count, item) => (count[item] = count[item] + 1 || 1, count), {});
+   
+  }else{
+   // const strArr = value.split(" ");
+    return value.toLowerCase().split(" ").sort().join(" ").match(/(.)\1+/g).length; 
+   
+    // for (let i = 0; i < strArr.length; i++) {
+    //   if (strArr.indexOf(strArr[i]) !== strArr.lastIndexOf(strArr[i])) {
+    //     if (!res.includes(strArr[i])) {
+    //       res.push(strArr[i]);
+    //     };
+    //   };
+    // };
+    // return res.join(" ");
+  }
+};
 
+console.log("with count: Duplicates: ", countDuplicatedValues(stringArr))
 
-// console.log("abc Duplicates: ", findDuplicatedArrayValues(stringArr),
-//   "abc Non Duplicated Values: ", findNonDuplicatedArrayValues(stringArr))
-
+console.log("with count: Duplicates: ", countDuplicatedValues(strWord))
+  
